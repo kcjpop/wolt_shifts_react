cedbeecd191dc03c887781eaacca56b670226248
Jin <snowytrees182@gmail.com>
2018-09-26 22:41:46 +0300

    added onClick booking shift API call

--- client/src/components/contents/AvailShifts.js
+++ client/src/components/contents/AvailShifts.js
@@ -1,137 +1,128 @@
-import React, {Component} from 'react'
-import moment from 'moment'
-import _ from 'lodash'
+import React, { Component } from "react";
+import moment from "moment";
+import _ from "lodash";
 
-import AvailShiftsNav from './AvailShiftsNav'
-import Spinner from '../../images/spinner_green.svg'
+import AvailShiftsNav from "./AvailShiftsNav";
+import Spinner from "../../images/spinner_green.svg";
 
-import AvailShiftsRow from './AvailShiftsRow'
+import AvailShiftsRow from "./AvailShiftsRow";
 // import AvailShiftsItem from './AvailShiftsItem'
 // import AvailShiftsItemHeader from './AvailShiftsItemHeader'
 
-
-
-
-
 class AvailShifts extends Component {
   state = {
-    cityObj : {}
+    cityObj: {}
   };
 
   async componentDidMount() {
-      console.log('compo did mount')
-      const res = await this._callApi()
-      await this.setState({ cityObj: res })
+    console.log("compo did mount");
+    const res = await this._callApi();
+    await this.setState({ cityObj: res });
   }
-
-
 
   // Tell webpack to proxy our API requests to our API server,
   _callApi = async () => {
-    const response = await fetch('/shifts');
+    const response = await fetch("/shifts");
     // console.log('GET response:', response)
     const shifts = await response.json();
     // console.log('shifts:', shifts)
     if (response.status !== 200) throw Error(shifts.message);
 
+    const orderedShifts = _.orderBy(shifts, "startTime", "asc");
+    const now = new Date().getTime();
+    const orderedShiftsFromNow = orderedShifts.filter(
+      shift => shift.startTime > now
+    );
+    const cities = await [
+      ...new Set(orderedShiftsFromNow.map(item => item.area))
+    ].sort();
 
-
-
-
-    const orderedShifts = _.orderBy(shifts,'startTime','asc');
-    const cities = await [...new Set(orderedShifts.map(item => item.area))].sort();
-
-    let cityObj = {}
+    let cityObj = {};
     await cities.map(city => {
-      cityObj[city] = orderedShifts.filter(shift => shift.area === city)
-    })
+      cityObj[city] = orderedShiftsFromNow.filter(shift => shift.area === city);
+    });
     // console.log('cityObj',cityObj)
 
-    const groupByDateObj = await this._groupByDates(cityObj)
+    const groupByDateObj = await this._groupByDates(cityObj);
     // console.log('groupByDateObj',groupByDateObj)
 
     return groupByDateObj;
-  }
-
-
-
+  };
 
   _groupByDates = cityObj => {
-    const groupByDateObj = {}
+    const groupByDateObj = {};
     for (let key in cityObj) {
-      groupByDateObj[key] = cityObj[key].reduce(function (newObj, shift) {
-         const formatted = moment(shift.startTime).format('LL');
-         newObj[formatted] = newObj[formatted] || [];
-         newObj[formatted].push(shift);
-         return newObj;
-       }, Object.create(null))
+      groupByDateObj[key] = cityObj[key].reduce(function(newObj, shift) {
+        const formatted = moment(shift.startTime).format("LL");
+        newObj[formatted] = newObj[formatted] || [];
+        newObj[formatted].push(shift);
+        return newObj;
+      }, Object.create(null));
     }
-    return groupByDateObj
-  }
-
-
-
+    return groupByDateObj;
+  };
 
   _renderAvailNavs = () => {
-    const {cityObj} = this.state
-    const navList = []
+    const { cityObj } = this.state;
+    const navList = [];
     for (let cityKey in cityObj) {
-      navList.push(<AvailShiftsNav {...this.props.match} key={cityKey} cityName={cityKey}/>)
+      navList.push(
+        <AvailShiftsNav
+          {...this.props.match}
+          key={cityKey}
+          cityName={cityKey}
+        />
+      );
     }
-    return navList
-  }
-
-
-
+    return navList;
+  };
 
   _renderAvailShiftsRows = () => {
-    const { cityObj } = this.state
-    const { pathname } = this.props.location
-    const cityName = pathname.replace('/availshift/','');
+    const { cityObj } = this.state;
+    const { pathname } = this.props.location;
+    const cityName = pathname.replace("/availshift/", "");
 
-    const shiftRows = []
-    if (cityName !== '/availshift') {
+    const shiftRows = [];
+    if (cityName !== "/availshift") {
       for (let dateKey in cityObj[cityName]) {
         // console.log(cityObj[cityName][dateKey])
-        const shortDate = dateKey.replace(', 2018','')
+        const shortDate = dateKey.replace(", 2018", "");
         shiftRows.push(
           <AvailShiftsRow
             key={shortDate}
             eachDateObjList={cityObj[cityName][dateKey]}
-            date={shortDate} shiftAmount={cityObj[cityName][dateKey].length}
+            date={shortDate}
+            shiftAmount={cityObj[cityName][dateKey].length}
             _bookShift={this._bookShift}
-           />)
+          />
+        );
       }
     }
+    return shiftRows;
+  };
 
-    // console.log(shiftRows)
-    return shiftRows
-
-  }
-
-  _bookShift = async (_id) => {
-    const resp = await fetch(`/shifts/${_id}/book`, {method: 'POST'})
-    console.log('Book resp:', resp)
-  }
-
-
-
+  _bookShift = async (_id, e) => {
+    console.log("e:", e.target.className);
+    console.log("_id:", _id);
+    const resp = await fetch(`/shifts/${_id}/book`, { method: "POST" });
+    console.log("Book resp:", resp);
+    const classValue = e.target.className;
+    if (resp.status === 200) {
+      // classValue = "btn btn-cancel";
+    }
+  };
 
   render() {
     // console.log('props:', this.props)
     return (
-        <div>
+      <div>
         <section class="shifts">
-          <nav className="shifts__nav">
-            {this._renderAvailNavs()}
-          </nav>
+          <nav className="shifts__nav">{this._renderAvailNavs()}</nav>
 
           {this._renderAvailShiftsRows()}
 
-             <div class="shifts__rows">
-
-
-                {/* <div class="shifts_row">
+          <div class="shifts__rows">
+            {/* <div class="shifts_row">
                     <header class="shifts__header">
                         <span class="shifts__text shifts__text--size-big">September 21</span>
                     </header>
@@ -146,11 +137,11 @@ class AvailShifts extends Component {
 
                     </div>
                 </div> */}
-            </div>
+          </div>
         </section>
-        </div>
-    )
+      </div>
+    );
   }
 }
 
-export default AvailShifts
+export default AvailShifts;
--- client/src/components/contents/AvailShiftsItem.js
+++ client/src/components/contents/AvailShiftsItem.js
@@ -1,41 +1,62 @@
-import React from 'react'
-import moment from 'moment'
+import React from "react";
+import moment from "moment";
 
-const _btnColor = (bookStatus) => (
-  bookStatus ? 'btn btn-cancel' : 'btn btn-book'
-)
+const _btnColor = bookStatus =>
+  bookStatus ? "btn btn-cancel" : "btn btn-book";
 
 const _checkTime = () => {
-  const currentTime = new Date().getTime()
-  console.log(currentTime)
-}
+  const currentTime = new Date().getTime();
+  console.log(currentTime);
+};
 
+let disabled = false;
 
-// const _btnStatus = (bookStatus) => {
-//   return !bookStatus && 'btn-disabled'
-// }
+const _btnDisabled = (bookStatus, startTime) => {
+  if (bookStatus) {
+    disabled = true;
+    return "btn-disabled";
+  }
+};
 
-// const _bookShift = () => {
-//
-// }
+const _disableReason = bookStatus => {
+  if (bookStatus) {
+    return "booked";
+  }
+};
 
-const AvailShiftsItem = ({shift, startTime, endTime, bookStatus, _bookShift}) => {
+const AvailShiftsItem = ({
+  shift,
+  startTime,
+  endTime,
+  bookStatus,
+  _bookShift
+}) => {
   return (
-      <div className="shifts__content">
-          <div className="avail-shifts__content-left">
-              <span className="shifts__time">{startTime}-{endTime}</span>
-          </div>
-          <div className="avail-shifts__content-right">
-              <span className="shifts__status shifts__status-booked">{ bookStatus ? 'booked' : '?' }</span>
-              <button
-                className={_btnColor(bookStatus)}
-                onClick={_bookShift.bind(this, shift.id)}
-                >
-                { bookStatus ? 'cancel' : 'book' }
-              </button>
-          </div>
+    <div className="shifts__content">
+      <div className="avail-shifts__content-left">
+        <span className="shifts__time">
+          {startTime}-{endTime}
+        </span>
       </div>
-  )
-}
+      <div className="avail-shifts__content-right">
+        <span className="shifts__status shifts__status-booked">
+          {_disableReason(bookStatus)}
+        </span>
+        {/* An here is the btn !!!! */}
+        <button
+          className={`${_btnColor(bookStatus)} ${_btnDisabled(
+            bookStatus,
+            shift.startTime
+          )}`}
+          onClick={_bookShift.bind(this, shift.id)}
+          disabled={disabled}
+        >
+          {/* An here is the btn !!!! */}
+          {bookStatus ? "cancel" : "book"}
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default AvailShiftsItem
+export default AvailShiftsItem;
